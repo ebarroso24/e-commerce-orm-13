b41e884315d27073e658f2b8b0207070dc52b28e
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-10 19:48:01 -0500

    added code to models routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -11,14 +11,14 @@ Product.init(
   {
     // Define columns for the product table
     id: {
-      type: DataTypes.INTEGER, // Set the data type to INTEGER
-      allowNull: false, // Disallow NULL values
-      primaryKey: true, // Set as the primary key
-      autoIncrement: true, // Automatically increment the value for each new record
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
     },
     product_name: {
-      type: DataTypes.STRING, // Set the data type to STRING
-      allowNull: false, // Disallow NULL values
+      type: DataTypes.STRING, 
+      allowNull: false, 
     },
     price: {
       type: DataTypes.DECIMAL, 
